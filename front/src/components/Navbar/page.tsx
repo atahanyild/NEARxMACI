0ccf35536a24f19f0fc40353f6103b1b9a516992
Eyüp Bay <eyupbay7@gmail.com>
2024-04-13 18:47:59 +0300

    navbar

--- front/src/components/Navbar/page.tsx
+++ front/src/components/Navbar/page.tsx
@@ -7,8 +7,8 @@ import Logo from "@/assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="bg-white h-[70px] border border-bottom border-[#F0F0F1] flex justify-center items-center">
-      <div className="w-[85%] flex justify-between items-center">
+    <div className="bg-white h-[70px] border-b-2  border-[#F0F0F1] flex justify-center items-center">
+      <div className="w-full h-full pl-8 flex justify-between items-center">
         <div>
           <div className="md:hidden">
             <Image src={Icon} width={54} height={54} alt="icon" />
@@ -17,8 +17,11 @@ const Navbar = () => {
             <Image src={Logo} width={140} height={140} alt="logo" />
           </div>
         </div>
-        <div>
-          <button className="bg-[#1B1B1B] rounded-3xl text-white px-5 py-2 font-normal">
+        <div className="h-full">
+          <button className="h-full text-black px-8 py-2 font-medium border-[#F0F0F1] border-l-[1.5px] hover:bg-[#F0F0F1]">
+            Polls
+          </button>
+          <button className="h-full text-black px-8 py-2 font-medium border-[#F0F0F1] border-l-[1.5px] hover:bg-[#F0F0F1]">
             Connect Wallet
           </button>
         </div>
