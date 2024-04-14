2064dc7fad47f765f3a86c939e6c76244a0b6169
Sena Eser <senaeser@Sena-MacBook-Air-3.local>
2024-04-14 10:04:16 +0200

    updated

--- front/src/components/VotingLoading.jsx
+++ front/src/components/VotingLoading.jsx
@@ -1,13 +1,11 @@
 "use client";
 
 export default function LoadingComp() {
-  console.log("LoadingComp");
-
   return (
     <div className="fixed inset-x-0 bottom-0 flex items-end justify-end z-50">
       <div className="flex w-40 h-16 animate-pulse bg-[#131313] bg-opacity-50 rounded-tl-xl">
         <div className="relative">
-          <div className="absolute bottom-3 -right-24 font-semibold text-lg text-white">
+          <div className="absolute bottom-3 -right-24 font-normal text-lg text-white">
             Voting..
           </div>
           <div className="absolute -bottom-8 -right-52">
